11b5bc91ff6ddfa2ad6836a5862554bfe2f0a073
DeeEmDy <dy-lanarias@hotmail.com>
2024-09-04 13:36:24 -0600

    a
    a

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0257530ee948edbb70edbae0e6f12c3e352d9f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13e8bb3c8996fadf6e7c79abcfe38020a5445295")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
